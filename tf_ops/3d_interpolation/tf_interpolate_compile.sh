3d2ffe9b18a47351f7b9f9820f7758b51ea4d78d
LaureenK <laureen@rkfinanz.de>
2021-11-21 20:33:52 +0100

    .

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -8,14 +8,14 @@ g++ -std=c++11 tf_interpolate.cpp \
 -o tf_interpolate_so.so \
 -shared \
 -fPIC \
--I$TF_INC/external/nsync/public
+-I$TF_INC/external/nsync/public \
 -I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include \
 -I/usr/local/cuda-8.0/include \
 -I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include/external/nsync/public \
 -lcudart \
 -L/usr/local/cuda-8.0/lib64/ \
 -L$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow \
--ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+-ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0 \
 -L$TF_LIB -ltensorflow_framework
 
 #g++ -std=c++11 tf_interpolate.cpp \
