45218ac50e0195041ed65028f03c5bfe644167dd
LaureenK <laureen@rkfinanz.de>
2021-11-21 20:39:34 +0100

    .

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,4 +1,5 @@
 # TF1.4 Python 3.5 CUDA 8.0
+# TF1.4 Python 3.7 CUDA 8.0
 
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
@@ -8,15 +9,13 @@ g++ -std=c++11 tf_interpolate.cpp \
 -o tf_interpolate_so.so \
 -shared \
 -fPIC \
--I$TF_INC/external/nsync/public \
--I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include \
+-I$TF_INC \
 -I/usr/local/cuda-8.0/include \
--I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include/external/nsync/public \
+-I$TF_INC/external/nsync/public \
 -lcudart \
 -L/usr/local/cuda-8.0/lib64/ \
--L$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow \
--ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0 \
--L$TF_LIB -ltensorflow_framework
+-L$TF_LIB \
+-ltensorflow_framework
 
 #g++ -std=c++11 tf_interpolate.cpp \
 #-o tf_interpolate_so.so \
