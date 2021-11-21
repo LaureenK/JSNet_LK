6e26dd70a4ce6b2089b55e95532c6d6c0cad08d8
LaureenK <laureen@rkfinanz.de>
2021-11-21 21:07:34 +0100

    Change SH Files TF_OPS

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -1,15 +1,19 @@
+# TF1.4 Python 3.5 CUDA 8.0
+# TF1.4 Python 3.7 CUDA 11.1
+
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+
 #/bin/bash
-/usr/local/cuda-8.0/bin/nvcc tf_sampling_g.cu \
+/usr/local/cuda-11.1/bin/nvcc tf_sampling_g.cu \
 -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-
-# TF1.4 Python 3.5 CUDA 8.0
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o \
 -o tf_sampling_so.so \
 -shared -fPIC \
--I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include \
--I/usr/local/cuda-8.0/include \
--I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include/external/nsync/public \
--lcudart -L/usr/local/cuda-8.0/lib64/ \
--L$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow \
+-I$TF_INC \
+-I/usr/local/cuda-11.1/include \
+-I$TF_INC/external/nsync/public \
+-lcudart -L/usr/local/cuda-11.1/lib64/ \
+-L$TF_LIB \
 -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
