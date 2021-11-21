d74966e124fe1dd254af03fbbaefd1d8e6dc09a8
LaureenK <laureen@rkfinanz.de>
2021-11-21 20:07:42 +0100

    change sh

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,10 +1,14 @@
 # TF1.4 Python 3.5 CUDA 8.0
 
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+
 #g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 g++ -std=c++11 tf_interpolate.cpp \
 -o tf_interpolate_so.so \
 -shared \
 -fPIC \
+-I$TF_INC/external/nsync/public
 -I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include \
 -I/usr/local/cuda-8.0/include \
 -I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include/external/nsync/public \
@@ -12,3 +16,16 @@ g++ -std=c++11 tf_interpolate.cpp \
 -L/usr/local/cuda-8.0/lib64/ \
 -L$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow \
 -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+-L$TF_LIB -ltensorflow_framework
+
+#g++ -std=c++11 tf_interpolate.cpp \
+#-o tf_interpolate_so.so \
+#-shared \
+#-fPIC \
+#-I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include \
+#-I/usr/local/cuda-8.0/include \
+#-I$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow/include/external/nsync/public \
+#-lcudart \
+#-L/usr/local/cuda-8.0/lib64/ \
+#-L$HOME/anaconda3/envs/tensorflow_1.4/lib/python3.5/site-packages/tensorflow \
+#-ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
