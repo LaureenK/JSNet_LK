a3f5c2e1a1d08389553350296f54760e20060d05
LaureenK <laureen@rkfinanz.de>
2021-11-22 18:22:00 +0100

    k

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -5,7 +5,7 @@ TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 #/bin/bash
-/usr/local/cuda-11.1/bin/nvcc tf_sampling_g.cu \
+/usr/local/cuda-11.1/bin/nvcc ~/neural_networks/jsnet/JSNet_LK/tf_ops/grouping/tf_sampling_g.cu \
 -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o \
