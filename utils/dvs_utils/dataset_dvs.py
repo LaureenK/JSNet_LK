5e0d71efb971694cb73ba879c966bf6cdc68daa5
LaureenK <laureen@rkfinanz.de>
2021-11-29 15:37:46 +0100

    k

--- utils/dvs_utils/dataset_dvs.py
+++ utils/dvs_utils/dataset_dvs.py
@@ -65,9 +65,9 @@ def data_sample(data_sample_queue, input_list, split, epoch, num_works, block_po
 
                 for dsi in data_sem_ins:
                     print("dsi shape: ")
-                    print(dsi[0])
-                    print(dsi[1])
-                    print(dsi[2])
+                    print(dsi[0].shape)
+                    print(dsi[1].shape)
+                    print(dsi[2].shape)
                     shuffle_dsi = provider.shuffle_data(*dsi)
                     data_sample_queue.put(shuffle_dsi)
                     del dsi
