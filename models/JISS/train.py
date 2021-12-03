4a4fe8c3f28b967e258d548c5a8547a9107a0a48
LaureenK <laureen@rkfinanz.de>
2021-12-03 11:20:30 +0100

    j

--- models/JISS/train.py
+++ models/JISS/train.py
@@ -204,6 +204,7 @@ def train_one_epoch(sess, ops, train_writer, dataset, epoch):
 
     for batch_idx in range(num_batches):
         current_data, current_sem, current_label = dataset.get_batch(False)
+        print("Batch size: ", BATCH_SIZE, " \nOne Batch:\nData: ", current_data.shape, " SegLabel: ", current_sem.shape, " InsLabel: ", current_label)
         feed_dict = {ops['pointclouds_pl']: current_data,
                      ops['labels_pl']: current_label,
                      ops['sem_labels_pl']: current_sem,
