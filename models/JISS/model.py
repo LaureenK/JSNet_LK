c7e566c5a07c0dcf83156d890217903c5a77d7f7
LaureenK <laureen@rkfinanz.de>
2021-12-06 12:22:01 +0100

    d

--- models/JISS/model.py
+++ models/JISS/model.py
@@ -27,10 +27,10 @@ def get_model(point_cloud, is_training, num_class, num_embed=5, sigma=0.05, bn_d
     l0_points = point_cloud[:, :, 3:]
     end_points['l0_xyz'] = l0_xyz
 
-    radius1 = 2
-    radius2 = 4
-    radius3 = 8
-    radius4 = 16
+    radius1 = 4
+    radius2 = 8
+    radius3 = 16
+    radius4 = 32
 
     # shared encoder
     l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=1024, radius=radius1, nsample=32, mlp=[32, 32, 64], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, is_dist=is_dist, scope='layer1')
