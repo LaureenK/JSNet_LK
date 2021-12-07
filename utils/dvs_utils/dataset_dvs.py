82f055bb223315d0b11daddb3a366e045fec280a
LaureenK <laureen@rkfinanz.de>
2021-12-07 17:25:17 +0100

    d

--- utils/dvs_utils/dataset_dvs.py
+++ utils/dvs_utils/dataset_dvs.py
@@ -255,7 +255,7 @@ class DVSDataset():
 
         if(input_list_txt == 'none'):
             if(split == 'train'):
-                print("Test1")
+                print("Dataset_dir : ", DATASET_TRAIN_DIR)
                 self.files_to_use = glob.glob(os.path.join(DATASET_TRAIN_DIR, split, "*.csv"))
             elif(split == 'validation'): 
                 self.files_to_use = glob.glob(os.path.join(DATASET_VALIDATION_DIR, split, "*.csv"))
