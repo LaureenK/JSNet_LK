c771b6f0c0bd49120e49016b8025b89b0f204a1d
LaureenK <laureen@rkfinanz.de>
2021-12-08 12:51:21 +0100

    start estimate change

--- models/JISS/estimate_mean_ins_size_Test.py
+++ models/JISS/estimate_mean_ins_size_Test.py
@@ -1,9 +1,14 @@
 # To estimate the mean instance size of each class in training set
 import os
 import sys
+import glob
 import numpy as np
 from scipy import stats
 import argparse
+
+DATASET_TRAIN_DIR = "/bigdata_hdd/klein/FrKlein_PoC/data/TrainFiles/"
+DATASET_VALIDATION_DIR = "/bigdata_hdd/klein/FrKlein_PoC/data/ValidationFiles/"
+DATASET_TEST_DIR = "/bigdata_hdd/klein/FrKlein_PoC/data/TestFiles/"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
@@ -14,17 +19,17 @@ import provider
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_root', type=str, default='data', help='data dir')
-parser.add_argument('--dataset', type=str, default='S3DIS', help='dataset [S3DIS]')
+parser.add_argument('--dataset', type=str, default='DVS', help='dataset [S3DIS]')
 parser.add_argument('--input_list', type=str, default='data/train_hdf5_file_list_woArea5.txt', help='estimate the mean instance size')
-parser.add_argument('--num_cls', type=int, default=13, help='estimate the mean instance size')
+parser.add_argument('--num_cls', type=int, default=4, help='estimate the mean instance size')
 parser.add_argument('--out_dir', type=str, default='log5', help='log dir to save mean instance size [model path]')
 FLAGS = parser.parse_args()
 
 
 def estimate(flags):
     num_classes = flags.num_cls
-    if flags.dataset == 'S3DIS':
-        train_file_list = [os.path.join(flags.data_root, line.strip()) for line in open(flags.input_list, 'r')]
+    if flags.dataset == 'DVS':
+        train_file_list = glob.glob(os.path.join(DATASET_TRAIN_DIR, "*.csv"))
     else:
         print("Error: Not support the dataset: ", flags.dataset)
         return
@@ -32,19 +37,12 @@ def estimate(flags):
     mean_ins_size = np.zeros(num_classes)
     ptsnum_in_gt = [[] for itmp in range(num_classes)]
 
-    for h5_filename in train_file_list:
-        print(h5_filename)
-        cur_data, cur_group, _, cur_sem = provider.loadDataFile_with_groupseglabel_stanfordindoor(h5_filename)
-        print("Cur_Data Shape: ", cur_data.shape)
-        print("cur_group Shape: ", cur_group.shape)
-        print("cur_sem Shape: ", cur_sem.shape)
+    for dvs_filename in train_file_list:
+        print(dvs_filename)
+        cur_data, cur_group, _, cur_sem = provider.loadDataFile_with_groupseglabel_stanfordindoor(dvs_filename)
         cur_data = np.reshape(cur_data, [-1, cur_data.shape[-1]])
         cur_group = np.reshape(cur_group, [-1])
         cur_sem = np.reshape(cur_sem, [-1])
-
-        print("Cur_Data Shape: ", cur_data.shape)
-        print("cur_group Shape: ", cur_group.shape)
-        print("cur_sem Shape: ", cur_sem.shape)
 
         un = np.unique(cur_group)
         for ig, g in enumerate(un):
